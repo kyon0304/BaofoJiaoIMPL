fb6902331c5ebd0ba978b01ecff09467d505be91
kyon <kyon0304@gmail.com>
2013-10-03 16:28:18 +0800

    quick sort in place in python

---
+++ quick_sort.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import random
+import pdb
+
+def unsorted(length):
+    return [random.randrange(0, length*10) for r in xrange(length)]
+
+def partition(l, start, end):
+    pivot = l[end]
+    top = end
+    bottom = start - 1
+    done = False
+
+    while not done:
+        while not done:
+            bottom += 1
+            if bottom >= top:
+                done = True
+                break
+            if l[bottom] > pivot:
+                l[top] = l[bottom]
+                break
+    
+        while not done:
+            top -= 1
+            if top <= bottom:
+                done = True
+                break
+            if l[top] < pivot:
+                l[bottom] = l[top]
+                break
+
+    l[top] = pivot
+    return top
+
+def quick_sort(l, start, end):
+    if start < end:
+        split = partition(l, start, end)
+        quick_sort(l, start, split - 1)
+        quick_sort(l, split + 1, end)
+    else:
+        return
+
+if __name__ == '__main__':
+    l = unsorted(10)
+    print l
+    start = 0
+    end = len(l) - 1
+    quick_sort(l, start, end)
+    print l
